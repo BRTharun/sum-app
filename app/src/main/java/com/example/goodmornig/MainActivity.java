49aa3d57300b37457bfc5bb105dc96a82fbaae5e
B.R.Tharun kumar <brtarunkumar18@gmail.com>
2021-10-12 13:41:50 +0530

    sum app

---
+++ app/src/main/java/com/example/goodmornig/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.goodmornig;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText n1,n2;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        textView = findViewById(R.id.textView);
+        n1=findViewById(R.id.n1);
+        n2=findViewById(R.id.n2);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int sum = Integer.parseInt(n1.getText().toString())+Integer.parseInt(n2.getText().toString());
+                textView.setText("sum of two numbers="+sum);
+            }
+        });
+    }
+}
